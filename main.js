efdec59408c48740c890efb359855a3061a8a1d1
Official-Fan-Zone <jarhead.tylerfisher@gmail.com>
2025-11-01 00:12:48 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+const themeBtn = document.getElementById('themeButton');
+themeBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+});
+console.log('FanCardShop initialized');
